682a498aa4edd773bc7a3ed8915fc95eb6bfe3d7
Kim <Kpampusch@yahoo.com>
2017-06-07 00:39:12 -0500

    compiled 3d unit tests into a single test

--- unit_tests/3d_math/main.c
+++ unit_tests/3d_math/main.c
@@ -3,66 +3,109 @@
 #include <NUS/NUS_library.h>
 
 #define PROGRAM_NAME "unit_test-3d_math"
-
-char nus_axes_cmp(NUS_axes, NUS_axes, float);//TODO: impliment in library
-char nus_matrix_cmp(NUS_matrix, NUS_matrix, float);//TODO: impliment in library
-
+#define UNIT_TEST_LOG_ERROR( ... )		\
+  printf("UNIT_TEST_LOG::ERROR::" __VA_ARGS__)
 
 int main(int argc, char *argv[])
 {
   NUS_vector test_vector;
+  NUS_quaternion test_quaternion;
   NUS_axes test_axes;
+  NUS_matrix test_matrix;
   printf("starting unit test %s\n", PROGRAM_NAME);
   if(argc){}
   if(argv){}
-  
-  test_vector = nus_vector_build(0.0, 0.0, 1.0);
-  if(test_vector.x != 0.0 || test_vector.y != 0.0 || test_vector.z != 1.0){
-    printf("UNIT_TEST_ERROR::failed to build vector\n");
-    return -1;
-  }
-  
+
+  // Test vector math
+  // Test vector addition
   test_vector = nus_vector_add(nus_vector_build(3.0, 2.0, 1.0),
 			nus_vector_build(1.0, 3.3, 0.7071));
-  if(nus_vector_cmp(test_vector, nus_vector_build(4.0, 5.3, 1.7071), 0.00001) == 0){
-    printf("UNIT_TEST_ERROR::failed to add vectors\n");
+  if(nus_vector_cmp(test_vector, nus_vector_build(4.0, 5.3, 1.7071), 0.00001)
+     == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to add vectors\n");
     return -1;
   }
-  
+
+  // Test vector normalization
   test_vector = nus_vector_normalize(nus_vector_build(1.0, 0.0, 1.0));
-  if(nus_vector_cmp(test_vector, nus_vector_build(0.7071, 0.0, 0.7071), 0.00001) == 0){
-    printf("UNIT_TEST_ERROR::failed to normalize vector\n");
+  if(nus_vector_cmp(test_vector, nus_vector_build(0.7071, 0.0, 0.7071), 0.00001)
+     == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to normalize vector\n");
+    return -1;
+  }
+
+  // Test quaternion math
+  //TODO quaternion stuff
+  
+  // Test axes math
+  // Rotate axes by pi/2
+  test_axes = nus_axes_global_yaw(nus_axes_build(nus_vector_build(-1.0, 0.0, 0.0),
+						 nus_vector_build(0.0, 1.0, 0.0),
+						 nus_vector_build(0.0, 0.0, 1.0)),
+				  3.14159265359/2.0);
+  if(nus_axes_cmp(test_axes, nus_axes_build(nus_vector_build(0.0, 0.0, 1.0),
+					    nus_vector_build(0.0, 1.0, 0.0),
+					    nus_vector_build(1.0, 0.0, 0.0)),
+		  0.00001) == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to rotate axes\n");
+    return -1;
+  }
+
+  // Test matrix math
+
+  // Test translation matrix
+  test_matrix = nus_matrix_translation(nus_vector_build(7.3, -2.1, 0.77));
+  test_vector = nus_matrix_transform(test_matrix, nus_vector_build(-2.3, 1.0, 7.0));
+  if(nus_vector_cmp(nus_vector_build(5.0, -1.1, 7.77),
+		    test_vector, 0.00001) == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to translate vector\n");
+    return -1;
+  }
+
+  // Test rotation matrix 1
+  test_quaternion = nus_quaternion_unit(nus_vector_build(0.0, 1.0, 0.0), 0.0);
+  test_axes = nus_axes_build(nus_vector_build(0.0, 0.0, 1.0),
+			     nus_vector_build(0.0, 1.0, 0.0),
+			     nus_vector_build(1.0, 0.0, 0.0));
+  test_axes = nus_axes_global_rotation(test_axes, test_quaternion);
+  test_matrix = nus_matrix_rotation(test_axes);
+  test_vector = nus_matrix_transform(test_matrix, nus_vector_build(0.0, 0.0, 1.0));
+  if(nus_vector_cmp(nus_vector_build(0.0, 0.0, 1.0),
+		    test_vector, 0.00001) == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to use rotation matrix\n");
     return -1;
   }
   
-  //TODO: axes
-  test_axes = nus_axes_build(nus_vector_build(0.0, 0.0, -1.0),
+  // Test rotation matrix 2
+  test_quaternion = nus_quaternion_unit(nus_vector_build(0.0, 1.0, 0.0), 3.14159/2.0);
+  test_axes = nus_axes_build(nus_vector_build(0.0, 0.0, 1.0),
 			     nus_vector_build(0.0, 1.0, 0.0),
-			     nus_vector_build(-1.0, 0.0, 0.0));
-  if((nus_vector_cmp(test_axes.forward, nus_vector_build(0.0, 0.0, -1.0), 0.00001) &&
-      nus_vector_cmp(test_axes.upward, nus_vector_build(0.0, 1.0, 0.0), 0.00001) &&
-      nus_vector_cmp(test_axes.left, nus_vector_build(-1.0, 0.0, 0.0), 0.00001)) == 0){
-    printf("UNIT_TEST_ERROR::failed to build axes\n");
+			     nus_vector_build(1.0, 0.0, 0.0));
+  test_axes = nus_axes_global_rotation(test_axes, test_quaternion);
+  test_matrix = nus_matrix_rotation(test_axes);
+  test_vector = nus_matrix_transform(test_matrix, nus_vector_build(0.0, 0.0, 1.0));
+  if(nus_vector_cmp(nus_vector_build(-1.0, 0.0, 0.0),
+		    test_vector, 0.00001) == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to use rotation matrix\n");
     return -1;
   }
-
-  //todo
-  printf("\nBasic Axes yaw by pi/2:\n");
-  test_axes = nus_axes_global_yaw(test_axes, 3.14159265359/2.0);
-  nus_axes_print(test_axes);
-
-
-  //TODO: matrices
-
+  
+  // Test transformation matrix (ordering translation and rotation)
+  test_quaternion = nus_quaternion_unit(nus_vector_build(0.0, 1.0, 0.0), 3.14159/2.0);
+  test_axes = nus_axes_build(nus_vector_build(0.0, 0.0, 1.0),
+			     nus_vector_build(0.0, 1.0, 0.0),
+			     nus_vector_build(1.0, 0.0, 0.0));
+  test_axes = nus_axes_global_rotation(test_axes, test_quaternion);
+  test_vector = nus_vector_build(-1.5, 3.14159, 2.1);
+  test_matrix = nus_matrix_transformation(test_vector, test_axes);
+  test_vector = nus_matrix_transform(test_matrix, nus_vector_build(0.0, 0.0, 1.0));
+  if(nus_vector_cmp(nus_vector_build(-2.5, 3.14159, 2.1),
+		    test_vector, 0.00001) == NUS_FALSE){
+    UNIT_TEST_LOG_ERROR("failed to use rotation matrix\n");
+    return -1;
+  }
+  
   printf("Math tested successfully\n");
   printf("unit test %s completed\n", PROGRAM_NAME);
   return 0;
 }
-
-char nus_axes_cmp(NUS_axes axes_0, NUS_axes axes_1, float d)//TODO: impliment in library
-{
-  return nus_vector_cmp(axes_0.forward, axes_1.forward, d) &&
-    nus_vector_cmp(axes_0.upward, axes_1.upward, d) &&
-    nus_vector_cmp(axes_0.left, axes_1.left, d);
-}
-//char nus_matrix_cmp(NUS_matrix, NUS_matrix, float);//TODO: impliment in library
