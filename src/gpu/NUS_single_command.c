1cd257ea5ad487ec0135dabe10c4571e59fec2ec
Kim <Kpampusch@yahoo.com>
2017-06-09 20:40:02 -0500

    added commented version of single_command that may provide speed increase

--- src/gpu/NUS_single_command.c
+++ src/gpu/NUS_single_command.c
@@ -34,3 +34,37 @@ NUS_result nus_single_command_end
   vkResetCommandBuffer(nus_single_command_buffer, 0);
   return NUS_SUCCESS;
 }
+/*
+// Alt. code, potential (small) speed increase
+NUS_result nus_single_command_begin
+(NUS_queue_info queue_info, VkCommandBuffer *p_command_buffer)
+{
+  if(nus_single_command_buffer == VK_NULL_HANDLE){
+    nus_queue_info_add_buffer(queue_info, &nus_single_command_buffer);
+    nus_single_command_buffer_begin_info = (VkCommandBufferBeginInfo){
+      .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+      .pNext = NULL,
+      .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+      .pInheritanceInfo = NULL
+    };
+  } else {
+    // Instead of device wait, wait on semaphore (TODO create semaphore)
+    vkDeviceWaitIdle(queue_info.p_gpu->logical_device);
+    vkResetCommandBuffer(nus_single_command_buffer, 0);
+  }
+  vkBeginCommandBuffer(nus_single_command_buffer, &nus_single_command_buffer_begin_info);
+  *p_command_buffer = nus_single_command_buffer;
+  return NUS_SUCCESS;
+}
+NUS_result nus_single_command_end
+(NUS_queue_info queue_info)
+{
+  if(vkEndCommandBuffer(nus_single_command_buffer) != VK_SUCCESS){
+      NUS_LOG_ERROR("failed to record single command buffer\n");
+      return NUS_FAILURE;
+  }
+  nus_command_group_append(queue_info.p_command_group, nus_single_command_buffer);
+  nus_queue_info_submit(queue_info);
+  return NUS_SUCCESS;
+}
+*/
