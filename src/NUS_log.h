9e1d0eec9aa8f83f4bffe2e887654a83af48dd3a
Kim <Kpampusch@yahoo.com>
2017-07-11 02:51:43 -0500

    added warning macro

--- src/NUS_log.h
+++ src/NUS_log.h
@@ -3,10 +3,11 @@
 
 #include <stdio.h>
 
-#define NUS_LOG( ... )			\
+#define NUS_LOG( ... )				\
   printf("NUS_LOG::" __VA_ARGS__)
-
 #define NUS_LOG_ERROR( ... )			\
   NUS_LOG("ERROR::" __VA_ARGS__)
+#define NUS_LOG_WARNING( ... )			\
+  NUS_LOG("WARNING::" __VA_ARGS__)
 
 #endif /* NUS_LOG_H */
