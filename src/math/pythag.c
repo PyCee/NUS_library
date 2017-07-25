061547a5b0c126483be0a4e51c26fa4df17076a7
Kim <Kpampusch@yahoo.com>
2017-07-24 21:18:41 -0500

    fixed math problem

--- src/math/pythag.c
+++
@@ -1,8 +0,0 @@
-#include "pythag.h"
-#include <math.h>
-
-double pythag(float x, float y, float z)
-{
-	return sqrt(x * x + y * y + z * z);
-	// return q_rsqrt(x * x + y * y + z * z);
-}
--- src/math/pythag.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PYTHAG_H_
-#define _PYTHAG_H_
-
-double pythag(float, float, float);
-
-#endif /* _PYTHAG_H_ */
