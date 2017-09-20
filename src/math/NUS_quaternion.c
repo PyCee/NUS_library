a30dc9e4c5b76ce6ccd3f60ebb58a95c89775568
Kim <Kpampusch@yahoo.com>
2017-09-20 12:47:02 -0500

    cleaned up quaternion.c

--- src/math/NUS_quaternion.c
+++ src/math/NUS_quaternion.c
@@ -5,7 +5,7 @@
 
 NUS_quaternion nus_quaternion_build(double w, double x, double y, double z)
 {
-  return (NUS_quaternion){w, x, y, z};
+  return (NUS_quaternion){.w = w, .x = x, .y = y, .z = z};
 }
 NUS_quaternion nus_quaternion_pure(NUS_vector vector)
 {
@@ -16,7 +16,8 @@ NUS_quaternion nus_quaternion_unit(NUS_vector vector, double radians)
   
   double half_radians = radians / 2.0,
     sin_half_radians = sin(half_radians);
-  return nus_quaternion_build(cos(half_radians), sin_half_radians * vector.x,
+  return nus_quaternion_build(cos(half_radians),
+			      sin_half_radians * vector.x,
 			      sin_half_radians * vector.y,
 			      sin_half_radians * vector.z);
 }
@@ -28,8 +29,7 @@ NUS_quaternion nus_quaternion_conjugate(NUS_quaternion quaternion)
 NUS_quaternion nus_quaternion_h_product
 (NUS_quaternion quaternion_0,  NUS_quaternion quaternion_1)
 {
-  return nus_quaternion_build(
-			      /* h product w component */
+  return nus_quaternion_build(/* h product w component */
 			      quaternion_0.w * quaternion_1.w -
 			      quaternion_0.x * quaternion_1.x -
 			      quaternion_0.y * quaternion_1.y -
@@ -57,8 +57,7 @@ NUS_vector nus_quaternion_apply_rotation
   NUS_quaternion rotate = nus_quaternion_pure(vector),
     conjugate = nus_quaternion_conjugate(quaternion),
     result = nus_quaternion_h_product(nus_quaternion_h_product(quaternion,
-							       rotate),
-				      conjugate);
+							       rotate), conjugate);
   return nus_vector_build(result.x, result.y, result.z);
 }
 NUS_quaternion nus_quaternion_lerp
@@ -68,13 +67,13 @@ NUS_quaternion nus_quaternion_lerp
   t = (t < 0) ? 0 : t;
   double b = 1.0 - t;
   return nus_quaternion_normalize(nus_quaternion_build(quaternion_0.w * b +
-					       quaternion_1.w * t,
-					       quaternion_0.x * b +
-					       quaternion_1.x * t,
-					       quaternion_0.y * b +
-					       quaternion_1.y * t,
-					       quaternion_0.z * b +
-					       quaternion_1.z* t));
+						       quaternion_1.w * t,
+						       quaternion_0.x * b +
+						       quaternion_1.x * t,
+						       quaternion_0.y * b +
+						       quaternion_1.y * t,
+						       quaternion_0.z * b +
+						       quaternion_1.z* t));
 }
 NUS_quaternion nus_quaternion_slerp
 (NUS_quaternion quaternion_0, NUS_quaternion quaternion_1, double t)
@@ -97,14 +96,14 @@ NUS_quaternion nus_quaternion_slerp
 						       quaternion_0.y * wp +
 						       quaternion_1.y * wq,
 						       quaternion_0.z * wp +
-						       quaternion_1.z* wq));
+						       quaternion_1.z * wq));
 }
 NUS_quaternion nus_quaternion_normalize(NUS_quaternion quaternion)
 {
   double mag_sq = quaternion.w * quaternion.w +
     quaternion.x * quaternion.x + quaternion.y * quaternion.y +
     quaternion.z * quaternion.z;
-  if(0.00000001 >= mag_sq){
+  if(0.00001 >= mag_sq){
     return quaternion;
   }
   double inv_mag = 1.0 / sqrt(mag_sq);
@@ -113,6 +112,5 @@ NUS_quaternion nus_quaternion_normalize(NUS_quaternion quaternion)
 }
 void nus_quaternion_print(NUS_quaternion quaternion)
 {
-  printf("NUS_quaternion:\n{%f, %f, %f, %f}\n", quaternion.w, quaternion.x,
-	 quaternion.y, quaternion.z);
+  printf("{%f, %f, %f, %f}\n", quaternion.w, quaternion.x, quaternion.y, quaternion.z);
 }
